ceca72018a4537664244962988dce7a1a569d974
Bugan <dones.bugan@gmail.com>
2018-12-13 14:49:26 -0200

    Video 5.1

---
+++ Assets/Scripts/Personagem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+    private Animator controleAnimacao;
+    private float velocidade = 0.2f;
+	private void Start () {
+        this.controleAnimacao = this.GetComponent<Animator>();
+	}
+	
+	
+	private void Update () {
+        Vector3 direcao = new Vector3();
+        direcao.y = Input.GetAxis("Vertical");
+        direcao.x = Input.GetAxis("Horizontal");
+
+        this.transform.position += direcao * this.velocidade;
+        bool estouAndando = direcao.magnitude != 0;
+        this.controleAnimacao.SetBool("EstaAndando", estouAndando);
+        this.controleAnimacao.SetFloat("VelocidadeX", direcao.x);
+        this.controleAnimacao.SetFloat("VelocidadeY", direcao.y);
+    }
+}
